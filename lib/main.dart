34e2fe456a20d411860ede370af56ad75aba27fc
Ritesh Sinha <ritesh.sinha.737001@gmail.com>
2025-02-24 19:12:28 +0530

    going to deploy website

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
+      //Website going to be Deploy
       home: HomeView(),
     );
   }
