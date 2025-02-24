dda57622349dbc11dad18ccbdf31c1eece14ba47
Ritesh Sinha <ritesh.sinha.737001@gmail.com>
2025-02-24 18:54:34 +0530

    final changes done

--- lib/component/widget.dart
+++ lib/component/widget.dart
@@ -1,9 +1,17 @@
 import 'package:flutter/material.dart';
 
 //screen size
+
+//SizeBox with Height
 SizedBox gapH(double value) => SizedBox(height: value);
+
+//SizeBox with Width
 SizedBox gapW(double value) => SizedBox(width: value);
+
+//it contain MediaQuery with Height
 screenHeight(BuildContext context) => MediaQuery.of(context).size.height;
+
+//it contain MediaQuery with Width
 screenWidth(BuildContext context) => MediaQuery.of(context).size.width;
 
 // Custom Text
--- lib/home/home_controller.dart
+++ lib/home/home_controller.dart
@@ -3,21 +3,39 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class HomeController extends GetxController {
+  // Initialized left position
   var positionLeft = 0.0.obs;
+
+  // Initialized Right position
   var positionRight = 0.0.obs;
+
+  // For Disable Left Button while moving Left Direction
   var isDisableLeft = false.obs;
+
+  // For Disable Right Button while moving Right Direction
   var isDisableRight = false.obs;
 
   void movePosition(String pos, BuildContext context) {
     if (pos == "Right") {
+      //Total Size of a Container - Small Red Box Size
       positionLeft.value = screenWidth(context) * .9 - 100;
       positionRight.value = 0;
+
+      //Disable right button while moving "Right Diraction"
       isDisableRight.value = true;
+
+      //Left button must true
       isDisableLeft.value = false;
     } else if (pos == "Left") {
       positionLeft.value = 0;
+
+      //Total Size of a Container - Small Red Box Size
       positionRight.value = screenWidth(context) * .9 - 100;
+
+      //disable left button while moving "Left Diraction"
       isDisableLeft.value = true;
+
+      //Right button must true
       isDisableRight.value = false;
     }
   }
